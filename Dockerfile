bb2e5702d9aad074bce74d4e20313ba0a62352f9
Mathieu Lecarme <mlecarme@bearstech.com>
2018-09-25 23:19:00 +0200

    ssh env

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,7 @@ COPY entrypoint.sh /opt/entrypoint.sh
 
 VOLUME /secret
 ENV PRIVATE ""
+ENV SSH_AUTH_SOCK /secret/ssh-agent.sock
 
 ENTRYPOINT ["/opt/entrypoint.sh"]
 CMD ["bash"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ echo $PRIVATE | sed -e "s/\\n/\n/g" > /run/private
 chmod 400 /run/private
 ssh-agent -a /secret/ssh-agent.sock
 
-export SSH_AUTH_SOCK=/secret/ssh-agent.sock
 expect << EOF
   spawn ssh-add /run/private
   expect "Enter passphrase"
